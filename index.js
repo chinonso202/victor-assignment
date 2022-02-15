edbcc48ac607ab83b23da8da810105a56ceff387
chinonso202 <chinonsopius88@yahoo.com>
2022-02-15 17:58:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+
+
+
+// let pet= ["lion","tiger","goat", "zebra","leopard"]; // an array of objects
+//array functions
+
+// pet.push("cat") //adding to the end of an array
+// console.log(pet)
+
+//function 2
+// pet.unshift("elephants") //adding to the begining o an array
+// console.log(pet)
+
+// //function3
+// pet.pop() //deleting the last element
+// console.log(pet)
+
+// //function 4
+// pet[3]="kangaroo"
+// console.log(pet)
+// //funcion 5
+
+// pet.shift() //removing the first element
+// console.log(pet)
+//to print the first word in the aray
+// console.log(pet.length)
+
+// // function 6
+// const RemoveElement= pet.pop () //making remove elements constants so you get them.
+// //console.log(RemoveElement)
+
+// //functio 7 sortin elements
+// //console.log(pet.sort()) //geting elemet by alphebatical order
+// console.log(pet)
+
+// const todos=[
+//     {
+//     id:1,
+//     text:"take out trash",
+//     isCompleted:true
+
+// },
+// {
+// id:2,
+// text:"dentist appointments",
+// isCompleted:true
+
+// },
+// {
+// id:3,
+// text:"work",
+// isCompleted: false
+
+// }
+
+
+
+// ]
+//  console.log(todos)
+
+// const todoJSON=JSON.stringify(todos)
+// console.log(todoJSON)
+
+
+// function addNum(num =2,num2=4,num3=2,num4=1 ) {
+//     return(2+4+2+1)
+// }
+
+// console.log(addNum())
+
+// es6 version  of function using "return"
+// const addNum=(num1=2,num2=5,num3=7)  =>{
+//     return(num1+num2+num3)
+// }
+// console.log(addNum())
+
+// function substractNum(num1=3,num2=10,num3=15,num4=16){
+//     return(num1-num2-num3-num4)
+// }
+// console.log(substractNum())
+
+// const substractNum=(num1=20,num2=10,num3=15)=>{
+//     return(num1-num2-num3)
+// }
+// console.log(substractNum())
+
+// const todo =[
+//     {
+//         id:1,
+//         Text:"hello",
+//         item:"good"
+//     },
+//     {
+//         id:2,
+//         text:"love u",
+//         item:"not good"
+//     }
+// ]
+// const todoJSON=JSON.stringify(todo)
+// console.log(todoJSON)
+
+// for(let i =0; i<10 ;1++){
+// console.log("hey")
+// }
+
+// const myName=(num1="hello",num2="nonso")=>{
+//        return(hello+nonso)
+//      }
+//      console.log(myName())
+
+// function greeter(name){
+
+    // }
+    
+    // console.log(greeter("nonso"))
+    
+// function average(num1,num2,num3){
+// return(num1+num2+num3)/3;
+// }
+// console.log(average(4,4,2))
+// let text = "chinonso"
+function changeText(text){
+    return(text.toUpperCase())
+}
+console.log(changeText("nigeriology"))
+
+const myArray = text.split(" " );
+
+console.log(myArray("myArray"));
+//     return "Hello " + name;
+let text = "How are you doing today?";
+console.log(myArray)
+
+
